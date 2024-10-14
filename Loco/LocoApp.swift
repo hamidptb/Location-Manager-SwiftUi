b71d2c98e452c03e0d41c9089f751c0ead7c46a7
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2024-10-14 12:15:48 +0330

    Initial Commit

---
+++ Loco/LocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocoApp.swift
+//  Loco
+//
+//  Created by Hamid on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct LocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
